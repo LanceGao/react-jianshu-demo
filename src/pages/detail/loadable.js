9039283c1ae15e832c7781cfdb2cc5c4ecceb31d
LanceGao <1123764761@qq.com>
2021-02-23 23:20:44 +0800

    代码优化

--- src/pages/detail/loadable.js
+++ src/pages/detail/loadable.js
@@ -5,8 +5,8 @@ const LoadableComponent = Loadable({
   loader: () => import('./index.js'),
   loading: () => {
       return <div>加载中</div>
-      
   }
 });
 
-export default () => <LoadableComponent />
+const loadable = () => <LoadableComponent />;
+export default loadable;
